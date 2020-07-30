12517ad9276362da335af07fd7c626bfb1619e3c
Kuddleman <edl1155@gmail.com>
2020-07-30 15:08:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const products = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+]
+
+localStorage.setItem('products', JSON.stringify( products ))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Products Manager</h1>
     </div>
   );
 }
