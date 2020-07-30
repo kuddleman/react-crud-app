f83cf89b61f198cd3494267981b81b058ecfc2d9
Kuddleman <edl1155@gmail.com>
2020-07-30 16:08:37 -0700

    add getProducts method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const products = [
@@ -14,12 +14,46 @@ const products = [
 
 localStorage.setItem('products', JSON.stringify( products ))
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Products Manager</h1>
-    </div>
-  );
+class App extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.getProducts()
+  }
+
+  getProducts = () => {
+    const products = JSON.parse(localStorage.getItem('products'))
+    this.setState({ products })
+  }
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+        {
+          this.state.products.map( product =>{
+            return (
+              <div key={ product.name }>
+                <span>{ product.name }</span>
+                {` | `} 
+                <span>${ product.price }</span>
+                {` | `} 
+                <button>Delete</button>
+              </div>
+            )
+          } )
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
